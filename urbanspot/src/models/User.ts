b7f03ce0d959f2b1866b18c291335554e432dce5
psenciales <psenciales@cloudari.com>
2025-12-21 12:01:17 +0100

    fix: puntos añadidos a entidad user

--- urbanspot/src/models/User.ts
+++ urbanspot/src/models/User.ts
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: String,
     email: { type: String, required: true },
     provider: { type: String, required: true },
+    points: { type: Number, required: true, default: 0 },
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
